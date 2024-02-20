8f82287c829c3dae1e89b3951f2572c9769100c9
mehul1409 <mehulbansalswm1234@gmail.com>
2024-02-20 10:13:42 +0530

    add movable divs

--- dest/index.js
+++ dest/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("hii bro");
+function makeDraggable(elementId) {
+    const element = document.getElementById(elementId);
+    const header = element.querySelector('.header');
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+    header.addEventListener('mousedown', (event) => {
+        isDragging = true;
+        const boundingRect = element.getBoundingClientRect();
+        offsetX = event.clientX - boundingRect.left;
+        offsetY = event.clientY - boundingRect.top;
+    });
+    document.addEventListener('mousemove', (event) => {
+        if (isDragging) {
+            const x = event.clientX - offsetX;
+            const y = event.clientY - offsetY;
+            element.style.left = x + 'px';
+            element.style.top = y + 'px';
+        }
+    });
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+}
+makeDraggable('wrapper');
+makeDraggable('wrapper2');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hii bro");
+function makeDraggable(elementId: string) {
+    const element = document.getElementById(elementId)!;
+    const header = element.querySelector('.header') as HTMLElement;
+    
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+  
+    header.addEventListener('mousedown', (event: MouseEvent) => {
+      isDragging = true;
+      const boundingRect = element.getBoundingClientRect();
+      offsetX = event.clientX - boundingRect.left;
+      offsetY = event.clientY - boundingRect.top;
+    });
+  
+    document.addEventListener('mousemove', (event: MouseEvent) => {
+      if (isDragging) {
+        const x = event.clientX - offsetX;
+        const y = event.clientY - offsetY;
+        element.style.left = x + 'px';
+        element.style.top = y + 'px';
+      }
+    });
+  
+    document.addEventListener('mouseup', () => {
+      isDragging = false;
+    });
+  }
+  
+  makeDraggable('wrapper');
+  makeDraggable('wrapper2');
